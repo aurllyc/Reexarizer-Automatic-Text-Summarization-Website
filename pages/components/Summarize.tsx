96ca801d48b060a051aeebebce449a0a56220f38
Aurellia Nur Fitria <aur3l163@gmail.com>
2025-08-26 12:01:13 +0700

    fixing

--- pages/components/Summarize.tsx
+++ pages/components/Summarize.tsx
@@ -340,8 +340,8 @@ const Summarize: React.FC = () => {
             {/* Mode Paragraph */}
             {mode === "Paragraph" && keywords.length > 0 && (
               <div className="flex-col items-center space-x-2">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center mb-2">
+                <div className="flex items-center justify-between mb-2">
+                  <div className="flex items-center">
                     <label className="text-md font-medium">
                       Select Keywords
                     </label>
